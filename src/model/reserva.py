b5942d77ba53097da03f48d0c859773bb6ac5c65
Gabriela Gavi <gabrielagave27@gmail.com>
2025-10-20 16:31:15 -0300

    adicionando classe reserva no model

---
+++ src/model/reserva.py
@@ -0,0 +1,87 @@
+class Reserva:
+    def __init__(
+        self,
+        id_reserva: int = None,
+        cpf: str = None,
+        numero_quarto: int = None,
+        data_checkin: str = None,
+        data_checkout: str = None,
+        qtd_hospedes: int = None,
+        valor_total: float = None,
+        status: str = None,
+        criado_em: str = None
+    ):
+        self.set_id_reserva(id_reserva)
+        self.set_cpf(cpf)
+        self.set_numero_quarto(numero_quarto)
+        self.set_data_checkin(data_checkin)
+        self.set_data_checkout(data_checkout)
+        self.set_qtd_hospedes(qtd_hospedes)
+        self.set_valor_total(valor_total)
+        self.set_status(status)
+        self.set_criado_em(criado_em)
+
+    
+    def get_id_reserva(self) -> int:
+        return self.__id_reserva
+
+    def get_cpf(self) -> str:
+        return self.__cpf
+
+    def get_numero_quarto(self) -> int:
+        return self.__numero_quarto
+
+    def get_data_checkin(self) -> str:
+        return self.__data_checkin
+
+    def get_data_checkout(self) -> str:
+        return self.__data_checkout
+
+    def get_qtd_hospedes(self) -> int:
+        return self.__qtd_hospedes
+
+    def get_valor_total(self) -> float:
+        return self.__valor_total
+
+    def get_status(self) -> str:
+        return self.__status
+
+    def get_criado_em(self) -> str:
+        return self.__criado_em
+
+    
+    def set_id_reserva(self, id_reserva: int):
+        self.__id_reserva = id_reserva
+
+    def set_cpf(self, cpf: str):
+        self.__cpf = cpf
+
+    def set_numero_quarto(self, numero_quarto: int):
+        self.__numero_quarto = numero_quarto
+
+    def set_data_checkin(self, data_checkin: str):
+        self.__data_checkin = data_checkin
+
+    def set_data_checkout(self, data_checkout: str):
+        self.__data_checkout = data_checkout
+
+    def set_qtd_hospedes(self, qtd_hospedes: int):
+        self.__qtd_hospedes = qtd_hospedes
+
+    def set_valor_total(self, valor_total: float):
+        self.__valor_total = valor_total
+
+    def set_status(self, status: str):
+        self.__status = status
+
+    def set_criado_em(self, criado_em: str):
+        self.__criado_em = criado_em
+
+    
+    def to_string(self) -> str:
+        return (
+            f"Reserva {self.__id_reserva} - CPF: {self.__cpf} - "
+            f"Quarto: {self.__numero_quarto} - Check-in: {self.__data_checkin} - "
+            f"Check-out: {self.__data_checkout} - Hóspedes: {self.__qtd_hospedes} - "
+            f"Total: R${self.__valor_total:.2f} - Status: {self.__status} - Criada em: {self.__criado_em}"
+        )
