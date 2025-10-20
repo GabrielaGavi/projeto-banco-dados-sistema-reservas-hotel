40ad7bdbcb4d6aeddf9d0bfdc6d97cd6fb24df11
Gabriela Gavi <gabrielagave27@gmail.com>
2025-10-20 11:59:47 -0300

    adicionando classe hospede no model

---
+++ src/model/hospede.py
@@ -0,0 +1,36 @@
+class Hospede:
+    def __init__(self, cpf: str = None, nome: str = None, telefone: str = None, data_cadastro: str = None):
+        self.set_cpf(cpf)
+        self.set_nome(nome)
+        self.set_telefone(telefone)
+        self.set_data_cadastro(data_cadastro)
+
+    
+    def get_cpf(self) -> str:
+        return self.__cpf
+
+    def get_nome(self) -> str:
+        return self.__nome
+
+    def get_telefone(self) -> str:
+        return self.__telefone
+
+    def get_data_cadastro(self) -> str:
+        return self.__data_cadastro
+
+    
+    def set_cpf(self, cpf: str) -> None:
+        self.__cpf = cpf
+
+    def set_nome(self, nome: str) -> None:
+        self.__nome = nome
+
+    def set_telefone(self, telefone: str) -> None:
+        self.__telefone = telefone
+
+    def set_data_cadastro(self, data_cadastro: str) -> None:
+        self.__data_cadastro = data_cadastro
+
+    
+    def to_string(self) -> str:
+        return f"{self.__cpf} - {self.__nome} - {self.__telefone} - {self.__data_cadastro}"
